c38c16d0c82bf2723d72b7c02448fb5a9bc22f23
Argi Danurahadi <danu.argi@gmail.com>
2021-03-11 10:39:27 +0700

    uncomment pebble test & template test

--- extra/src/test/kotlin/id/yoframework/extra/extension/pebble/PebbleSpec.kt
+++ extra/src/test/kotlin/id/yoframework/extra/extension/pebble/PebbleSpec.kt
@@ -1,10 +1,15 @@
 package id.yoframework.extra.extension.pebble
 
+import id.yoframework.core.extension.resource.tmpDir
 import id.yoframework.core.extension.vertx.buildVertx
 import io.vertx.core.Vertx
+import io.vertx.core.buffer.Buffer
+import io.vertx.kotlin.coroutines.awaitResult
+import kotlinx.coroutines.runBlocking
 import org.amshove.kluent.shouldBeEqualTo
 import org.amshove.kluent.shouldNotBeNull
 import org.amshove.kluent.shouldNotBeNullOrBlank
+import org.apache.commons.lang3.RandomStringUtils
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
@@ -62,43 +67,41 @@ object PebbleSpec : Spek({
                 templateStrict.shouldNotBeNull()
             }
 
+            it("should able to compile file template") {
+                val randomSuffix = RandomStringUtils.randomAlphabetic(12)
+                val location = "${tmpDir()}/test_template-$randomSuffix.peb"
+                val templateString =
+                    """
+                   <html>
+                   <head>
+                        <title>{% block title %}Default Website{% endblock %}</title>
+                   </head>
+                   <body>
+                     <div id="content">
+                        {% block content %}{% endblock %}
+                     </div>
+                     <div id="footer">
+                        {% block footer %}
+                          Copyright 2014
+                        {% endblock %}
+                     </div>
+                   </body>
+                   </html>
+                """.trimIndent()
 
-            // temporarily disable this test before update core module version
-//            it("should able to compile file template") {
-//                val randomSuffix = RandomStringUtils.randomAlphabetic(12)
-//                val location = "${tmpDir()}/test_template-$randomSuffix.peb"
-//                val templateString =
-//                    """
-//                   <html>
-//                   <head>
-//                        <title>{% block title %}Default Website{% endblock %}</title>
-//                   </head>
-//                   <body>
-//                     <div id="content">
-//                        {% block content %}{% endblock %}
-//                     </div>
-//                     <div id="footer">
-//                        {% block footer %}
-//                          Copyright 2014
-//                        {% endblock %}
-//                     </div>
-//                   </body>
-//                   </html>
-//                """.trimIndent()
-//
-//                val templateLocation = runBlocking {
-//                    val fileSystem = vertx.fileSystem()
-//                    fileSystem.writeFile(location, Buffer.buffer(templateString))
-//                    location
-//                }
-//
-//                val template = Pebble.compileTemplate(templateLocation)
-//                template.shouldNotBeNull()
-//
-//                val templateStrict = Pebble.compileTemplate(templateLocation, true)
-//                templateStrict.shouldNotBeNull()
-//
-//            }
+                val templateLocation = runBlocking {
+                    val fileSystem = vertx.fileSystem()
+                    awaitResult<Void> { fileSystem.writeFile(location, Buffer.buffer(templateString), it) }
+                    location
+                }
+
+                val template = Pebble.compileTemplate(templateLocation)
+                template.shouldNotBeNull()
+
+                val templateStrict = Pebble.compileTemplate(templateLocation, true)
+                templateStrict.shouldNotBeNull()
+
+            }
 
             it("should able to compile classpath template") {
                 val templateLocation = "template/hello.peb"
--- extra/src/test/kotlin/id/yoframework/extra/extension/pebble/TemplateSpec.kt
+++ extra/src/test/kotlin/id/yoframework/extra/extension/pebble/TemplateSpec.kt
@@ -1,10 +1,15 @@
 package id.yoframework.extra.extension.pebble
 
 import com.mitchellbosecke.pebble.PebbleEngine
+import id.yoframework.core.extension.resource.tmpDir
 import id.yoframework.core.extension.vertx.buildVertx
 import io.vertx.core.Vertx
+import io.vertx.core.buffer.Buffer
+import io.vertx.kotlin.coroutines.awaitResult
+import kotlinx.coroutines.runBlocking
 import org.amshove.kluent.shouldNotBeNull
 import org.amshove.kluent.shouldNotBeNullOrBlank
+import org.apache.commons.lang3.RandomStringUtils
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
@@ -37,49 +42,47 @@ object TemplateSpec : Spek({
             }
         }
 
+        on("handling file template") {
+            val randomSuffix = RandomStringUtils.randomAlphabetic(12)
+            val location = "${tmpDir()}/test_template-$randomSuffix.peb"
+            val templateString =
+                """
+                   <html>
+                   <head>
+                        <title>{% block title %}Default Website{% endblock %}</title>
+                   </head>
+                   <body>
+                     <div id="content">
+                        {% block content %}{% endblock %}
+                     </div>
+                     <div id="footer">
+                        {% block footer %}
+                          Copyright 2014
+                        {% endblock %}
+                     </div>
+                   </body>
+                   </html>
+                """.trimIndent()
 
-        // temporarily throw exception if coroutine context is null before update core module version
-//        on("handling file template") {
-//            val randomSuffix = RandomStringUtils.randomAlphabetic(12)
-//            val location = "${tmpDir()}/test_template-$randomSuffix.peb"
-//            val templateString =
-//                """
-//                   <html>
-//                   <head>
-//                        <title>{% block title %}Default Website{% endblock %}</title>
-//                   </head>
-//                   <body>
-//                     <div id="content">
-//                        {% block content %}{% endblock %}
-//                     </div>
-//                     <div id="footer">
-//                        {% block footer %}
-//                          Copyright 2014
-//                        {% endblock %}
-//                     </div>
-//                   </body>
-//                   </html>
-//                """.trimIndent()
-//
-//            val templateLocation = runBlocking {
-//                val fileSystem = vertx.fileSystem()
-//                fileSystem.writeFile(location, Buffer.buffer(templateString))
-//                location
-//            }
-//
-//            it("should able to load template") {
-//                val compiledTemplate = engine.getTemplate(templateLocation)
-//                compiledTemplate.shouldNotBeNull()
-//            }
-//
-//            it("should able to evaluate template") {
-//                val compiledTemplate = engine.getTemplate(templateLocation)
-//                val result = StringWriter()
-//                val parameters = mapOf("title" to "String Title", "content" to "Content", "footer" to "this is footer")
-//                compiledTemplate.evaluate(result, parameters)
-//                result.toString().shouldNotBeNullOrBlank()
-//            }
-//        }
+            val templateLocation = runBlocking {
+                val fileSystem = vertx.fileSystem()
+                awaitResult<Void> { fileSystem.writeFile(location, Buffer.buffer(templateString), it) }
+                location
+            }
+
+            it("should able to load template") {
+                val compiledTemplate = engine.getTemplate(templateLocation)
+                compiledTemplate.shouldNotBeNull()
+            }
+
+            it("should able to evaluate template") {
+                val compiledTemplate = engine.getTemplate(templateLocation)
+                val result = StringWriter()
+                val parameters = mapOf("title" to "String Title", "content" to "Content", "footer" to "this is footer")
+                compiledTemplate.evaluate(result, parameters)
+                result.toString().shouldNotBeNullOrBlank()
+            }
+        }
 
         on("handling literal template") {
             val templateString =
